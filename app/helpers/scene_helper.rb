245fea2d8123e8c23c1bbf4317595dd05369bc2c
Raymond Fallon <Raymond.Fallon@pennmedicine.upenn.edu>
2022-05-04 22:21:25 -0400

    Refactor random_chunks

--- app/helpers/scene_helper.rb
+++ app/helpers/scene_helper.rb
@@ -11,14 +11,14 @@ module SceneHelper
 
   def random_chunks(line_segment)
     words = line_segment.split(' ')
-    return ['', line_segment, ''] unless words.size > 2
+    return ['', line_segment, ''] unless words.size > 3
 
-    idx_1 = (0..(words.size - 4)).to_a.sample
-    idx_2 = (idx_1..(words.size)).to_a.sample
+    idx_1 = (0..(words.size - 3)).to_a.sample
+    idx_2 = ((idx_1+1)..(words.size)).to_a.sample
     chunk_1 = words.slice(0, idx_1).join(' ')
     chunk_2 = words.slice(idx_1, idx_2 - idx_1).join(' ')
     chunk_3 = words[idx_2..].join(' ')
-    
+
     [chunk_1, chunk_2, chunk_3]
   end
 end
