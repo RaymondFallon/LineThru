6935ce0f55b6cc4991a5815c1489352fe8b0e970
Raymond Fallon <raymond.fallon@pennmedicine.upenn.edu>
2022-11-10 18:07:57 -0500

    Add some character avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def character_photo(character)
+    set = (character.id % 4) + 1
+    photo_url = "https://robohash.org/set_set#{set}/#{character.id}?bgset=bg#{(set % 2) + 1}"
+    tag.img(src: photo_url, class: 'character-photo')
+  end
+
   def fa_icon(name, klass = '', fa_type = 'fas')
     tag.i('', class: "#{fa_type} fa-fw fa-#{name} #{klass}", 'aria-hidden': 'true')
   end
